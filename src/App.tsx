cf42604761670e87d1d84ff54763d9931bef485e
dv-zinke <50859352+dv-zinke@users.noreply.github.com>
2019-05-22 17:28:53 +0900

    삭제 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,61 @@ class App extends React.Component {
   state ={
    number:0, 
    inputValue: '',
-   todolist:[
-     
-   ]
+   todolist:[]
   };
+  input:any = null;
+    /**
+     * 인풋 체인지이벤트
+     * @param {React.FormEvent<HTMLInputElement>} e
+     */
   inputOnChange = (e: React.FormEvent<HTMLInputElement>) =>{
     this.setState({
       inputValue:e.currentTarget.value
     });
   };
+    /**
+     * 인풋클릭이벤트
+     */
   buttonClick = () =>{
     const {number, inputValue, todolist} = this.state;
+    if(inputValue === ''){
+        console.log(this.input);
+        return alert("입력하지않으셨습니다");
+    }
     this.setState({
       inputValue:'',
       number: number+1,
-      todolist:[...todolist, {id:number, value:inputValue}]
-    })
-  }
+      todolist:[...todolist, {id:number, value:inputValue, complete:false}]
+    });
+
+
+
+  };
+    /**
+     * 삭제클릭이벤트
+     */
+  deleteClick = (e: React.FormEvent<HTMLButtonElement>) =>{
+    const {todolist} = this.state;
+    const parent:any = e.currentTarget.parentNode;
+    this.setState({
+       todolist: todolist.filter((todo:any) => {return todo.value !== parent.children[0].innerText})
+    });
+  };
+    /**
+     * 완료클릭이벤트
+     */
+  completeClick = () =>{
+      console.log("완료클릭");
+  };
 
 
   public render() {
-    const todolistMake = () =>this.state.todolist.map((todo:any)=>{return <Todolist todo={todo.value}/>})
+    const todolistMake = () =>this.state.todolist.map((todo:any)=>{return <Todolist key={todo.id} todo={todo.value} deleteClick={this.deleteClick} completeClick={this.completeClick}/>});
     
     return (
       <div className="App">
          <Header/>
-         <Input changeEvent={this.inputOnChange} clickEvent={this.buttonClick}/>
+         <Input ref={ref=>{this.input =ref}} changeEvent={this.inputOnChange} clickEvent={this.buttonClick}/>
          <div className="todolist_container">
             {todolistMake()}
          </div>
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 
 interface Props {
-   todo:string
+   todo:string,
+   deleteClick(e:React.FormEvent<HTMLButtonElement>):void,
+   completeClick():void
   }
   
 class Todolist extends React.Component<Props> {
     public render() {
-        const {todo} = this.props;
+        const {todo, deleteClick, completeClick} = this.props;
         return (
             <div className="todolist">
-               {todo}
-               <button>삭제</button>
+               <div className="value">{todo}</div>
+               <button onClick={completeClick}>완료</button>
+               <button onClick={deleteClick}>삭제</button>
             </div>
         );
     }
