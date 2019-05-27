b5b8fa78ed474ba1763a1526964714edffaf2722
dv-zinke <50859352+dv-zinke@users.noreply.github.com>
2019-05-27 21:59:42 +0900

    디자인 쉽게 하기 위해서 ant design 플러그인설치
    TodoInput 적용
    다른부분적용해야됨

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,114 @@
-import * as React from 'react';
-import './App.css';
-import Input from "./components/Input";
-import Header from "./components/Header";
-import Todolist from './components/Todolist';
-
-
+import * as React from "react"
+import "./App.css"
+import TodoInput from "./components/TodoInput"
+import Header from "./components/Header"
+import Todolist from "./components/Todolist"
 
 class App extends React.Component {
-  state ={
-   number:0, 
-   inputValue: '',
-   todolist:[]
-  };
-    /**
-     * 인풋 체인지이벤트
-     * @param {React.FormEvent<HTMLInputElement>} e
-     */
-  private inputOnChange = (e: React.FormEvent<HTMLInputElement>) =>{
+  state = {
+    number: 0,
+    inputValue: "",
+    todolist: []
+  }
+  /**
+   * 인풋 체인지이벤트
+   * @param {React.FormEvent<HTMLInputElement>} e
+   */
+  private inputOnChange = (e: React.FormEvent<HTMLInputElement>) => {
     this.setState({
-      inputValue:e.currentTarget.value
-    });
-  };
-    /**
-     * 인풋클릭이벤트
-     */
-  private  buttonClick = () =>{
-    const {number, inputValue, todolist} = this.state;
-    
-    if(inputValue === ''){
-        this.refInput.focus();
-        return alert("입력하지않으셨습니다");
+      inputValue: e.currentTarget.value
+    })
+  }
+  /**
+   * 인풋클릭이벤트
+   */
+  private buttonClick = () => {
+    const { number, inputValue, todolist } = this.state
+
+    if (inputValue === "") {
+      this.refInput.focus()
+      return alert("입력하지않으셨습니다")
     }
     this.setState({
-      inputValue:'',
-      number: number+1,
-      todolist:[...todolist, {id:number, value:inputValue, complete:false}]
-    });
+      inputValue: "",
+      number: number + 1,
+      todolist: [...todolist, { id: number, value: inputValue, complete: false }]
+    })
 
-    this.refInput.value = '';
-  
-  };
-    /**
-     * 삭제클릭이벤트
-     */
-  private deleteClick = (e: React.FormEvent<HTMLButtonElement>) =>{
-    const {todolist} = this.state;
-    const parent:any= e.currentTarget.parentNode;
-    this.setState({
-       todolist: todolist.filter((todo:any) => {return todo.value !== parent.children[0].innerText})
-    });
-  };
-    /**
-     * 완료클릭이벤트
-     */
-  private completeClick = (e:React.FormEvent<HTMLButtonElement>) =>{
-     const {todolist} = this.state;
-   
-      const parent:any= e.currentTarget.parentNode;
-
-     const index = todolist.findIndex((todo:any) =>{return todo.value === parent.children[0].innerText})
-     const selectTodolist:any = todolist[index];
-
-     const nextTodolist:any = [...todolist];
-     if(selectTodolist.complete) {
-      e.currentTarget.innerHTML = '완료';
-     }else {
-      e.currentTarget.innerHTML = '취소';
-     }
-
-     nextTodolist[index] = {
-       ...selectTodolist,
-       complete: !selectTodolist.complete
-     };
-
-     this.setState({
-       todolist:nextTodolist
-     })
-       
-  };
-  private refInput:HTMLInputElement;
-  private InputRefEvent = (input:HTMLInputElement) =>{
-    this.refInput = input;
+    this.refInput.state.value = ""
   }
-  
+  /**
+   * 삭제클릭이벤트
+   */
+  private deleteClick = (e: React.FormEvent<HTMLButtonElement>) => {
+    const { todolist } = this.state
+    const parent: any = e.currentTarget.parentNode
+    this.setState({
+      todolist: todolist.filter((todo: any) => {
+        return todo.value !== parent.children[0].innerText
+      })
+    })
+  }
+  /**
+   * 완료클릭이벤트
+   */
+  private completeClick = (e: React.FormEvent<HTMLButtonElement>) => {
+    const { todolist } = this.state
+
+    const parent: any = e.currentTarget.parentNode
+
+    const index = todolist.findIndex((todo: any) => {
+      return todo.value === parent.children[0].innerText
+    })
+    const selectTodolist: any = todolist[index]
+
+    const nextTodolist: any = [...todolist]
+    if (selectTodolist.complete) {
+      e.currentTarget.innerHTML = "완료"
+    } else {
+      e.currentTarget.innerHTML = "취소"
+    }
+
+    nextTodolist[index] = {
+      ...selectTodolist,
+      complete: !selectTodolist.complete
+    }
+
+    this.setState({
+      todolist: nextTodolist
+    })
+  }
+  private refInput: any
+  private InputRefEvent = (input: HTMLInputElement) => {
+    this.refInput = input
+  }
+
   public render() {
-    const todolistMake = () =>this.state.todolist.map((todo:any)=>{return <Todolist key={todo.id} complete={todo.complete} todo={todo.value} deleteClick={this.deleteClick} completeClick={this.completeClick}/>});
-    
+    const todolistMake = () =>
+      this.state.todolist.map((todo: any) => {
+        return (
+          <Todolist
+            key={todo.id}
+            complete={todo.complete}
+            todo={todo.value}
+            deleteClick={this.deleteClick}
+            completeClick={this.completeClick}
+          />
+        )
+      })
+
     return (
       <div className="App">
-         <Header/>
-         <Input refEvent={this.InputRefEvent} changeEvent={this.inputOnChange} clickEvent={this.buttonClick}/>
-         <div className="todolist_container">
-            {todolistMake()}
-         </div>
+        <Header />
+        <TodoInput
+          refEvent={this.InputRefEvent}
+          changeEvent={this.inputOnChange}
+          clickEvent={this.buttonClick}
+        />
+        <div className="todolist_container">{todolistMake()}</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,23 +1,31 @@
-import * as React from 'react';
-import './Todolist.css'
+import * as React from "react"
+import "./Todolist.css"
+import { Button } from "antd"
+
 interface Props {
-   todo:string,
-   complete:boolean,
-   deleteClick(e:React.FormEvent<HTMLButtonElement>):void,
-   completeClick(e:React.FormEvent<HTMLButtonElement>):void
-  }
-  
-class Todolist extends React.Component<Props> {
-    public render() {
-        const {todo, deleteClick, completeClick, complete} = this.props;
-        return (
-            <div className="todolist">
-               { complete ? <div className="value complete">{todo}</div> : <div className="value">{todo}</div>}
-               <button onClick={completeClick}>완료</button>
-               <button onClick={deleteClick}>삭제</button>
-            </div>
-        );
-    }
+  todo: string
+  complete: boolean
+  deleteClick(e: React.FormEvent<HTMLButtonElement>): void
+  completeClick(e: React.FormEvent<HTMLButtonElement>): void
 }
 
-export default Todolist;
+class Todolist extends React.Component<Props> {
+  public render() {
+    const { todo, deleteClick, completeClick, complete } = this.props
+    return (
+      <div className="todolist">
+        {complete ? (
+          <div className="value complete">{todo}</div>
+        ) : (
+          <div className="value">{todo}</div>
+        )}
+        <button onClick={completeClick}>완료</button>
+        <Button type="danger" className="deleteBtn" onClick={deleteClick}>
+          삭제
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default Todolist
